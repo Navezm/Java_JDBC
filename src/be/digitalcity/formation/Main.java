3513b0b1e9820e8807812c67a424eef22b43f63b
Navezm <navez.martin@gmail.com>
2021-08-05 11:10:37 +0200

    Demo done

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -1,16 +1,16 @@
 package be.digitalcity.formation;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import be.digitalcity.formation.model.Section;
+
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:postgresql://localhost:5432/DBSlide";
-        String username = "postgres";
-        String password = "123456";
+//        String url = "jdbc:postgresql://localhost:5432/DBSlide";
+//        String username = "postgres";
+//        String password = "123456";
 
 //        Connection co = null;
 //        try () {
@@ -23,9 +23,26 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-        System.out.println("Trying connection");
-        try (Connection co = DriverManager.getConnection(url, username, password);) { // La connexion sera auto fermée
-            System.out.println("Connection gotten");
+        try (Connection co = ConnectionFactory.getConnection()) { // La connexion sera auto fermée
+
+            Statement stmt = co.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM section");
+
+            while(rs.next()) {
+
+                Section s = new Section();
+//                s.setId(rs.getLong("section_id"));
+                s.setId(rs.getLong(1));
+//                s.setName(rs.getString("section_name"));
+                s.setName(rs.getString(2));
+//                s.setDelegateId(rs.getInt("delegate_id"));
+                s.setDelegateId(rs.getInt(3));
+
+                System.out.println(s.getId() + " " +
+                        s.getName() + " " +
+                        s.getDelegateId());
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
